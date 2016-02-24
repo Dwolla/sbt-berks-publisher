63c69725698c36740002e56d8ddfefd2020ee584
Brian P. Holt <bholt@dwolla.com>
2016-02-23 18:02:23 -0600

    make tasks from settings so DwollaDockerApp can set them based on its tasks

--- src/main/scala/com/dwolla/sbt/berks/BerksPublisherKeys.scala
+++ src/main/scala/com/dwolla/sbt/berks/BerksPublisherKeys.scala
@@ -5,8 +5,8 @@ import sbt._
 trait BerksPublisherKeys {
   lazy val Berks = config("berks")
 
-  lazy val projectName = settingKey[String]("name of project name in stack.json") in Berks
+  lazy val projectName = TaskKey[String]("projectName", "name of project, used to calculate the S3 key") in Berks
   lazy val packageName = settingKey[String]("pathless filename for Berks package") in Berks
   lazy val s3Bucket = settingKey[String]("S3 bucket where the Berks package should be uploaded") in Berks
-  lazy val s3Key = settingKey[String]("S3 key where the Berks package should be uploaded") in Berks
+  lazy val s3Key = TaskKey[String]("s3Key", "S3 key where the Berks package should be uploaded") in Berks
 }
